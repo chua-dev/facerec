6eb832c011b8a2db1b39a3d8f7996bcdbbc245f5
chua <chua3940@gmail.com>
2021-10-20 16:55:34 +0800

    multiple video media select success

--- app/javascript/packs/script3-16.js
+++ app/javascript/packs/script3-16.js
@@ -2,11 +2,6 @@
 import * as faceapi from 'face-api.js'
 //import * as tf from `@tensorflow/tfjs`
 
-console.log('work')
-let theVid = document.getElementById('inputVideo')
-theVid.msHorizontalMirror = true
-theVid.play()
-
 Promise.all([
   faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
   //faceapi.nets.ssdMobilenetv1.loadFromUri('/models'),
@@ -24,38 +19,102 @@ const text = document.getElementById("realname")
 const exp = document.getElementById("exp")
 const age = document.getElementById("age")
 
-// CHECK AVAILABLE DEVICE
+///////////// CHECK AVAILABLE DEVICE
 
-navigator.mediaDevices.enumerateDevices().then((devices) => {
-  let videoSourcesSelect = document.getElementById("video-source");
-  let audioSourcesSelect = document.getElementById("audio-source");
+let videoSourcesSelect = document.getElementById("video-source");
+let audioSourcesSelect = document.getElementById("audio-source");
 
-  // Iterate over all the list of devices (InputDeviceInfo and MediaDeviceInfo)
-  devices.forEach((device) => {
-      let option = new Option();
-      option.value = device.deviceId;
+// Create Helper to ask for permission and list devices
+let MediaStreamHelper = {
+    // Property of the object to store the current stream
+    _stream: null,
+    // This method will return the promise to list the real devices
+    getDevices: function() {
+        return navigator.mediaDevices.enumerateDevices();
+    },
+    // Request user permissions to access the camera and video
+    requestStream: function() {
+        if (this._stream) {
+            this._stream.getTracks().forEach(track => {
+                track.stop();
+            });
+        }
 
-      // According to the type of media device
-      switch(device.kind){
-          // Append device to list of Cameras
-          case "videoinput":
-              option.text = device.label || `Camera ${videoSourcesSelect.length + 1}`;
-              videoSourcesSelect.appendChild(option);
-              break;
-          // Append device to list of Microphone
-          case "audioinput":
-              option.text = device.label || `Microphone ${videoSourcesSelect.length + 1}`;
-              audioSourcesSelect.appendChild(option);
-              break;
-      }
+        const audioSource = audioSourcesSelect.value;
+        const videoSource = videoSourcesSelect.value;
+        const constraints = {
+            audio: {
+                deviceId: audioSource ? {exact: audioSource} : undefined
+            },
+            video: {
+                deviceId: videoSource ? {exact: videoSource} : undefined
+            }
+        };
+    
+        return navigator.mediaDevices.getUserMedia(constraints);
+    }
+};
 
-      console.log(device);
+MediaStreamHelper.requestStream().then(function(stream){
+  console.log(stream);
+  // Store Current Stream
+  MediaStreamHelper._stream = stream;
+
+  // Select the Current Streams in the list of devices
+  audioSourcesSelect.selectedIndex = [...audioSourcesSelect.options].findIndex(option => option.text === stream.getAudioTracks()[0].label);
+  videoSourcesSelect.selectedIndex = [...videoSourcesSelect.options].findIndex(option => option.text === stream.getVideoTracks()[0].label);
+
+  // Play the current stream in the Video element
+  //videoPlayer.srcObject = stream;
+  
+  // You can now list the devices using the Helper
+  MediaStreamHelper.getDevices().then((devices) => {
+      // Iterate over all the list of devices (InputDeviceInfo and MediaDeviceInfo)
+      devices.forEach((device) => {
+          let option = new Option();
+          option.value = device.deviceId;
+
+          // According to the type of media device
+          switch(device.kind){
+              // Append device to list of Cameras
+              case "videoinput":
+                  option.text = device.label || `Camera ${videoSourcesSelect.length + 1}`;
+                  videoSourcesSelect.appendChild(option);
+                  break;
+              // Append device to list of Microphone
+              case "audioinput":
+                  option.text = device.label || `Microphone ${videoSourcesSelect.length + 1}`;
+                  audioSourcesSelect.appendChild(option);
+                  break;
+          }
+
+          console.log(device);
+      });
+  }).catch(function (e) {
+      console.log(e.name + ": " + e.message);
   });
-}).catch(function (e) {
-  console.log(e.name + ": " + e.message);
-});
+}).catch(function(err){
+  console.error(err);
+}); 
 
-// END AVAILABLE DEVICE
+
+let theVid = document.getElementById('inputVideo')
+
+videoSourcesSelect.onchange = function(){
+  MediaStreamHelper.requestStream().then(function(stream){
+      MediaStreamHelper._stream = stream;
+      theVid.srcObject = stream;
+  });
+};
+
+audioSourcesSelect.onchange = function(){
+  MediaStreamHelper.requestStream().then(function(stream){
+      MediaStreamHelper._stream = stream;
+      theVid.srcObject = stream;
+  });
+};
+
+///////////// END AVAILABLE DEVICE
 
 var faceMatcher = undefined
 var labelWithImage = undefined
